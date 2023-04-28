7781fd7677107c5b1c09c546fa418ac6768201f0
hyuns22 <115512607+hyuns22@users.noreply.github.com>
2023-04-28 17:39:25 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.py
@@ -0,0 +1,12 @@
+def solution(n):
+    N = str(n)
+    N_li = list(N)
+    answer = 0
+    for i in N_li:
+        answer += int(i)
+    
+
+    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    print('Hello Python')
+
+    return answer
