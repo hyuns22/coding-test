5d07cd517b6d774e2a08ca9ba5f99a95f486bbb3
hyuns22 <115512607+hyuns22@users.noreply.github.com>
2023-06-29 05:04:16 +0900

    [Bronze V] Title: ??!, Time: 108 ms, Memory: 12796 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String ID;
+        Scanner sc = new Scanner(System.in);
+        ID = sc.next();
+        System.out.println(ID+"??!");
+
+    }
+}
