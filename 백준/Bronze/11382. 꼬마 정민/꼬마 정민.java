73adb41b37f9961992c9216c74a97776a4fa61d9
hyuns22 <115512607+hyuns22@users.noreply.github.com>
2023-08-04 22:29:04 +0900

    [Bronze V] Title: 꼬마 정민, Time: 208 ms, Memory: 17784 KB -BaekjoonHub

--- 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -1,13 +1,15 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        long a, b, c;
         Scanner sc = new Scanner(System.in);
-        a = sc.nextLong();
-        b = sc.nextLong();
-        c = sc.nextLong();
-        System.out.println(a+b+c);
+        BigInteger A, B, C;
+
+        A = sc.nextBigInteger();
+        B = sc.nextBigInteger();
+        C = sc.nextBigInteger();
+        System.out.println(A.add(B).add(C));
 
     }
 }
