f7400a7544bc73bc93ae6b24217334b265ccde5a
hyuns22 <115512607+hyuns22@users.noreply.github.com>
2023-08-04 22:31:56 +0900

    [Bronze V] Title: 개, Time: 200 ms, Memory: 17368 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,14 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("|\\_/|\n" +
+                "|q p|   /}\n" +
+                "( 0 )\"\"\"\\\n" +
+                "|\"^\"`    |\n" +
+                "||_/=\\\\__|");
+
+    }
+}
