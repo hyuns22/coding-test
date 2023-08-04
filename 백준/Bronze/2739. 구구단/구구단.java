da731fc44841bb9cff076bd7634293e857b79eef
hyuns22 <115512607+hyuns22@users.noreply.github.com>
2023-08-04 22:43:10 +0900

    [Bronze V] Title: 구구단, Time: 212 ms, Memory: 17796 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,16 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A;
+        A = sc.nextInt();
+
+        for(int i = 1; i<10 ; i++)
+        {
+            System.out.printf("%d * %d = %d%n",A, i, A*i);
+        }
+
+    }
+}
