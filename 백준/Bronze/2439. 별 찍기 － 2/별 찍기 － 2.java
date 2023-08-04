b9dbcadd86b477f536805e487594a3ab87797a14
hyuns22 <115512607+hyuns22@users.noreply.github.com>
2023-08-04 23:45:19 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 276 ms, Memory: 15276 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String str = bf.readLine();
+        int T = Integer.parseInt(str);
+        for(int i=1; i<=T; i++)
+        {
+            for(int j = T; j>i; j--)
+            {
+                System.out.print(" ");
+            }
+            for(int j = 1; j<=i; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+
+
+    }
+}
