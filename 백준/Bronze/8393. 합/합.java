623480caf52ab16112a9fe86bbedd865035ec7d0
hyuns22 <115512607+hyuns22@users.noreply.github.com>
2023-08-04 22:45:53 +0900

    [Bronze V] Title: 합, Time: 208 ms, Memory: 17592 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,13 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A, B, T;
+        T = sc.nextInt();
+
+        System.out.println(T*(T+1)/2);
+
+    }
+}
