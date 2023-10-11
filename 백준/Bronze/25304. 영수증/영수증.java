d02fed5ac7f0976644896fb158a25d5eb5d4d0fb
hyuns22 <115512607+hyuns22@users.noreply.github.com>
2023-10-11 22:40:22 +0900

    [Bronze IV] Title: 영수증, Time: 220 ms, Memory: 17680 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.java
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -1,23 +1,19 @@
-import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int X, N;
-        X = sc.nextInt();
-        N = sc.nextInt();
-        int sum = 0;
-        for(int i=0; i<N; i++)
-        {
-            int a,b;
-            a= sc.nextInt();
-            b= sc.nextInt();
-            sum += a*b;
-        }
 
-        if(X==sum) System.out.println("Yes");
-        else System.out.println("No");
-
-    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int X = sc.nextInt();
+		int N = sc.nextInt();
+		
+		for(int i=0; i<N;i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			X-=a*b;
+		}
+		if(X==0) System.out.println("Yes");
+		else System.out.println("No");
+		sc.close();
+	}
 }
